8ded17e6692231244ba88e2956724eeb7d0ce64d
vidhik2002 <vidhik2002@gmail.com>
2021-10-20 19:26:23 +0530

    feat: added graph tools

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const { graphqlExpress } = require("apollo-server-express")
 // App
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
+app.use(express.urlencoded({ extended: true }));
+app.use("/graphql", express.json(), graphqlExpress({ schema: mySchema }));
 
 app.listen(process.env.PORT, () => {
   console.log("listening");
